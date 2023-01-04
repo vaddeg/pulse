dfec0fb0a10877698e011b9ee25d97460fc42c4f
Vad <example@gmail.com>
2023-01-04 15:36:19 +0300

    12 commit - add new file and work from project

--- src/js/script.js
+++ src/js/script.js
@@ -27,11 +27,25 @@ $(document).ready(function(){
           e.preventDefault();
           $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
           $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-        })
+        });
       });
     };
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
+    //Modal
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      });
+    });
   });
